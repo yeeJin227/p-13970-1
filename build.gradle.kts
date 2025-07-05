31e2037119b5006efd8b586dfd47f254d67fc2e0
yeeJin227 <dpwls99600@gmail.com>
2025-07-05 19:52:09 +0900

    add-sample-data

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,12 +24,20 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
     implementation("org.springframework.boot:spring-boot-starter-web")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.projectlombok:lombok")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
---
+++ src/main/java/com/back/domain/post/postComment/entity/PostComment.java
@@ -0,0 +1,30 @@
+package com.back.domain.post.postComment.entity;
+
+
+import com.back.domain.post.post.entity.Post;
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class PostComment extends BaseEntity {
+
+    private String content;
+
+    public void modify(String content){
+        this.content = content;
+    }
+
+    @ManyToOne
+    private Post post;
+
+    public PostComment(Post post, String content){
+        this.post = post;
+        this.content = content;
+    }
+
+}
